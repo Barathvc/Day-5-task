9c6ef7072077d68091200211c39e91d675a214c4
Your Name <you@example.com>
2024-02-23 08:54:38 +0530

    Day-5 task

---
+++ resumejon.js
@@ -0,0 +1,33 @@
+const myResume=[
+    {
+       "name":"Barath",
+       "mobile": 8682818030,
+    "email":"vcbarath02@gmail.com",
+    "degree": "B.E",
+    "location":{
+        "address":"Palaniyammal layout Pollachi",
+        "postal code":642001,
+        "village":"mahalingapuram",
+       "city":"Coimbatore"
+    },
+    "education":[
+        {
+            "institution":"Dr.mcet",
+            "dept":"EIE",
+            "BATCH":2024,
+            "cgpa":8.4,
+            "schoolings":"PKD",
+            "sslc percentage":78,
+            "hsc percentage":57,
+
+        },
+            
+    ],
+    "skills":[{
+        "programming":"Html,java,javascript",
+        "certification":"coursera certificate for c programming",
+
+    }]
+    }
+]
+console.log(myResume);
